b98a6f35cd3232b5269b9a5fdeac565d23f444bf
O-ekiri <e.obogakiri@gmail.com>
2022-03-15 20:07:57 +0100

    A program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "_putchar.c"
+#include "_putchar.h"
 /**
- * main - print _putchaar
+ * main - Prints _putchar
  *
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	return(0);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
